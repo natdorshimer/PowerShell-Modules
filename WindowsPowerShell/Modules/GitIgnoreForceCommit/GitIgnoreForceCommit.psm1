8588dabbf6f4a939abe5dae54263f62b09c05efc
Natalie Dorshimer <60105199+natdorshimer@users.noreply.github.com>
2020-09-08 16:18:30 -0400

    Update GitIgnoreForceCommit.psm1

--- WindowsPowerShell/Modules/GitIgnoreForceCommit/GitIgnoreForceCommit.psm1
+++ WindowsPowerShell/Modules/GitIgnoreForceCommit/GitIgnoreForceCommit.psm1
@@ -2,8 +2,7 @@ function GitIgnoreForceCommit()
 {
     <#
     .SYNOPSIS
-        If a gitignore is added retroactively or updated, this will force those ignores onto the project to allow them to be committed. 
-        Make sure to use this in the folder in which the repository is located.
+        If a gitignore is added retroactively or updated, this will force those ignores onto the project to allow them to be committed. Make sure to use this in the folder in which the repository is located.
     #>
     git rm -r --cached .
     git add .
